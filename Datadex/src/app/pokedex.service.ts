989ec6b6f8df1117d3f86c76f236933d664ab94d
simoncm <mollardsimon11@gmail.com>
2022-02-08 13:59:51 +0100

    minor fixes

--- Datadex/src/app/pokedex.service.ts
+++ Datadex/src/app/pokedex.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokedexService {
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
+
+  getData(url:string) {
+    return this.http.get(url)
+  }
 }
