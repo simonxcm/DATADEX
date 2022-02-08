4fd7b50469930e13297892dffcd04473afda7e19
aghilas <aghil.naitamer@gmail.com>
2022-02-08 11:31:58 +0100

    div card created

--- Datadex/src/app/app.module.ts
+++ Datadex/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PokedexHomepageComponent } from './pokedex-homepage/pokedex-homepage.component';
 import { PokemonItemComponent } from './component/pokemon-item/pokemon-item.component';
+import { PokemonListComponent } from './component/pokemon-list/pokemon-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PokedexHomepageComponent,
-    PokemonItemComponent
+    PokemonItemComponent,
+    PokemonListComponent
   ],
   imports: [
     BrowserModule,
