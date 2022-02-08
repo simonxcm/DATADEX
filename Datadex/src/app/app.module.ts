38a09eded710a7ef6f3eea90bcce3bf0749f860f
simoncm <mollardsimon11@gmail.com>
2022-02-08 13:57:22 +0100

    service creation for pokedex API fetch

--- Datadex/src/app/app.module.ts
+++ Datadex/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { PokedexHomepageComponent } from './pokedex-homepage/pokedex-homepage.component';
 import { PokemonItemComponent } from './component/pokemon-item/pokemon-item.component';
 import { PokemonListComponent } from './component/pokemon-list/pokemon-list.component';
+import { PokedexService } from './pokedex.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -16,9 +18,10 @@ import { PokemonListComponent } from './component/pokemon-list/pokemon-list.comp
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [PokedexService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
