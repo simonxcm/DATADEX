acafa1be35e8d17b71c736ac5021985ed8883aaa
aghilas <aghil.naitamer@gmail.com>
2022-02-08 14:28:37 +0100

    connection with api

--- Datadex/src/app/app.component.ts
+++ Datadex/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { ProviderAst } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { PokedexService} from './pokedex.service'
 
@@ -5,11 +6,20 @@ import { PokedexService} from './pokedex.service'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers:[PokedexService]
 })
 export class AppComponent implements OnInit {
+  pokemonName: any;
+  url= 'https://pokeapi.co/api/v2/pokemon/{name}'
+  constructor(private service: PokedexService){}
+  getPokemonName(): void{
+    this.service.getData(this.url).subscribe(data=>this.pokemonName=data)
+  }
+
 
   ngOnInit(): void {
+    this.getPokemonName
   }
 
   title = 'Datadex';
